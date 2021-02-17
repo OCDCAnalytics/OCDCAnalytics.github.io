b556db4da475267ff047314adf605a5499da97dd
david.j.brewer28.mil@gmail.com <david.j.brewer28.mil@gmail.com>
2021-02-17 15:43:59 -0800

    Nav page

--- Java.js
+++ Java.js
@@ -1,4 +1,3 @@
-<script>
 function myFunction() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
@@ -20,4 +19,25 @@ function myFunction() {
     }
   }
 }
-</script>
+
+function myFunction1() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput1");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable1");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
